2fc80e25c5728fc41b67113c9dd752644c9e3b1b
Finn Chen <finn.chen.maenhaut@gmail.com>
2022-12-14 11:23:24 -0500

    commit tree e71a8dc6c50fc1810936643ec1d5cfc2278c0b85

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+function appendCat(name) {
+  console.log([...cats, name]);
+  return [...cats, name];
+}
+
+appendCat("Mekkie");
+
+function prependCat(name) {
+  console.log([name, ...cats]);
+  return [name, ...cats];
+}
+function removeLastCat() {
+  const newCats = cats.slice();
+  console.log(newCats);
+  newCats.pop();
+  return newCats;
+}
+function removeFirstCat() {
+  const newCats = cats.slice();
+  newCats.shift();
+  return newCats;
+}
